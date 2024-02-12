32a93b1680e34a5fff3b7e794e47fa303f09c62d
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-02-12 00:54:02 -0500

    quito deteccion de color de fondo para letra

--- contentScript.js
+++ contentScript.js
@@ -3,14 +3,12 @@ window.onload = function() {
     
     if (/^https:\/\/github\.com\/[^\/]+\/[^\/]+/.test(window.location.href)) {
 
-      const colorDeFondo = window.getComputedStyle(document.body).backgroundColor;
-
       const $DEV = document.createElement('a');
       $DEV.classList.add('dev-leoSpj');
-      $DEV.style.color = colorDeFondo;
       $DEV.textContent = 'GitHub.dev';
 
       $DEV.href = window.location.href.replace('github.com', 'github.dev');
+      $DEV.target = '_blank';
 
       const $CONTENEDOR = document.querySelector('.bWpuBf');
       $CONTENEDOR.appendChild($DEV);
